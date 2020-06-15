177ad063bda5107338e642851892df66068267e6
CLM <chliming2016@163.com>
2020-06-15 08:55:32 +0800

    104,111：二叉树最大和最小深度

---
+++ leetcode/src/recursive/Solution111.java
@@ -0,0 +1,51 @@
+package recursive;//给定一个二叉树，找出其最小深度。
+//
+// 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。 
+//
+// 说明: 叶子节点是指没有子节点的节点。 
+//
+// 示例: 
+//
+// 给定二叉树 [3,9,20,null,null,15,7], 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7 
+//
+// 返回它的最小深度 2. 
+// Related Topics 树 深度优先搜索 广度优先搜索
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution111 {
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        return root.left == null || root.right == null ? left + right + 1 : Math.min(left, right) + 1;
+    }
+
+    public static void main(String[] args) {
+        TreeNode treeNode = new TreeNode(10);
+        treeNode.left = new TreeNode(5);
+        treeNode.right = new TreeNode(15);
+        treeNode.right.left = new TreeNode(6);
+        treeNode.right.right = new TreeNode(20);
+
+        Solution111 solution111 = new Solution111();
+        System.out.println(solution111.minDepth(treeNode));
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
