25c099f3d0648e889666f93fbf77bbb042b0c6f2
程立明 <chengliming@mcfly.com.cn>
2020-07-15 19:05:43 +0800

    96题：给定一个整数 n，求以 1 ... n 为节点组成的二叉搜索树有多少种？ 动态规划

---
+++ leetcode/src/ListNode.java
@@ -0,0 +1,13 @@
+/**
+ * @author Chengliming
+ * @create 2020-07-14 7:58 下午
+ **/
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
