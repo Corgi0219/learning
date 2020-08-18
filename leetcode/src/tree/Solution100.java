63f06b2c911cd6fc553cd30595f155b35b57b657
程立明 <chengliming@mcfly.com.cn>
2020-08-18 09:52:46 +0800

    每日一题

---
+++ leetcode/src/tree/Solution100.java
@@ -0,0 +1,80 @@
+package tree;//给定两个二叉树，编写一个函数来检验它们是否相同。 
+//
+// 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。 
+//
+// 示例 1: 
+//
+// 输入:       1         1
+//          / \       / \
+//         2   3     2   3
+//
+//        [1,2,3],   [1,2,3]
+//
+//输出: true 
+//
+// 示例 2: 
+//
+// 输入:      1          1
+//          /           \
+//         2             2
+//
+//        [1,2],     [1,null,2]
+//
+//输出: false
+// 
+//
+// 示例 3: 
+//
+// 输入:       1         1
+//          / \       / \
+//         2   1     1   2
+//
+//        [1,2,1],   [1,1,2]
+//
+//输出: false
+// 
+// Related Topics 树 深度优先搜索 
+// 👍 432 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution100 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode treeNode = new TreeNode(4);
+        treeNode.left = new TreeNode(1);
+        treeNode.left.left = new TreeNode(2);
+        treeNode.left.left.left = new TreeNode(3);
+
+        TreeNode treeNode1 = new TreeNode(4);
+        treeNode1.left = new TreeNode(1);
+        treeNode1.left.left = new TreeNode(2);
+//        treeNode1.left.left.left = new TreeNode(3);
+
+        System.out.println(new Solution100().isSameTree(treeNode, treeNode1));
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
